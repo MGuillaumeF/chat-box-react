b2465fc0201dbee5b59b249bd32c86ca17f6a8dc
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2020-04-19 09:22:49 +0200

    Add HashBrowser to upload chat

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom';
 import './App.css';
 import SignIn from './components/sign-in/SignIn';
 
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {HashRouter, Route, Switch} from 'react-router-dom';
 import App from './App';
 import Error404 from './components/error-pages/Error404';
 
 const Root = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Switch>
       <Route exact path='/' component={SignIn}></Route>
       <Route path='/pseudo/:pseudo' component={App}></Route>
       <Route component={Error404}></Route>
     </Switch>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 ReactDOM.render(<Root/>,
