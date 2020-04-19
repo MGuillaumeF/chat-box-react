d5d94478c30a201de9d6d988eaaf14aa4d7cb93f
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2020-04-19 09:03:32 +0200

    Updates

---
+++ precache-manifest.f739380aea8cd43847499208e20d3221.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe4f4f020bde2f1a4d2b3c1f5b85c7e4",
+    "url": "/chat-box-react/index.html"
+  },
+  {
+    "revision": "fcbe1632b3ee17299142",
+    "url": "/chat-box-react/static/css/main.4bb1cd5d.chunk.css"
+  },
+  {
+    "revision": "2ee3bb82c15b42b26046",
+    "url": "/chat-box-react/static/js/2.4267630e.chunk.js"
+  },
+  {
+    "revision": "73138c634db8d37252cbabcb33689813",
+    "url": "/chat-box-react/static/js/2.4267630e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcbe1632b3ee17299142",
+    "url": "/chat-box-react/static/js/main.f28346a0.chunk.js"
+  },
+  {
+    "revision": "d37e9f930d6537a32234",
+    "url": "/chat-box-react/static/js/runtime-main.bbdc70a0.js"
+  }
+]);
