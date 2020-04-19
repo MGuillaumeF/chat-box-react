5fb1e7a579aa8be5d32ca8f137d3e5b3b256da7b
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2020-04-19 05:53:32 +0200

    Add redirection after submit pseudo

--- src/components/sign-in/SignIn.tsx
+++ src/components/sign-in/SignIn.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export interface ISignInProps {
+    history: { push: Function }
 }
 
-export default function SignIn (props: ISignInProps) {
-  return (
-    <div className="connexionBox">
-      <form className="connexion">
-          <label></label>
-          <input type="text" name="" id="" required placeholder="Pseudo"/>
-          <button type="submit">Go</button>
-      </form>
-    </div>
-  );
+export default function SignIn(props: ISignInProps) {
+    const [pseudo, setPseudo] = useState('');
+    const onChange = (event: { target: { value: string } }) => {
+        setPseudo(event.target.value);
+    };
+    const onSubmit = (event: { preventDefault: Function }) => {
+        event.preventDefault();
+        props.history.push('/pseudo/' + pseudo);
+    };
+    return (
+        <div className="connexionBox">
+            <form className="connexion" onSubmit={onSubmit}>
+                <label></label>
+                <input type="text" value={pseudo} onChange={onChange} name="" id="" required placeholder="Pseudo" />
+                <button type="submit">Go</button>
+            </form>
+        </div>
+    );
 }
